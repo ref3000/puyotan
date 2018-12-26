c72569cb35ab4dc6d7df411ab330a831f2d706c2
refpuyo <refpuyo@gmail.com>
2018-12-26 09:32:19 +0900

    update

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import AppHeader from './AppHeader'
 import Game from './Game'
 
-export default () => (
+const App = () => (
   <div className='App'>
     <AppHeader />
     <Game />
   </div>
 )
+
+export default App;
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './AppHeader.css';
 
-export default () => (
+const AppHeader = () => (
   <div className='AppHeader'>
     <div className='AppHeader-title'>ぷよたんβ</div>
     <div className='AppHeader-version'>v0.1</div>
   </div>
 )
+
+export default AppHeader;
